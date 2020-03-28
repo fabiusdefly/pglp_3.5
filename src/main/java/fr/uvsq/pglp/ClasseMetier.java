15fb680c935b254b1c8aa95074077a71ec6a07a6
eloifabius <fabius1995>
2020-03-28 05:10:28 +0100

    création de la classe ClasseMetier

---
+++ src/main/java/fr/uvsq/pglp/ClasseMetier.java
@@ -0,0 +1,10 @@
+package fr.uvsq.pglp;
+
+public class ClasseMetier extends Affichage {
+    Affiche a;
+    public void UneMethodeMetier() {
+        Affich("Debut dune methode metier");
+    }
+
+}
+
